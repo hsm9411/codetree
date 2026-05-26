8d17263faed53192d78211560e8612eb488121d8
hsm9411 <210804140+hsm9411@users.noreply.github.com>
2026-05-26 17:34:05 +0900

    [Passed] 재귀함수를 이용한 최소공배수 (131ms, 10MB)

--- trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
+++ trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 10;
+    
+    public static int n;
+    public static int[] arr = new int[MAX_N + 1];
+    
+    // 두 수간의 최소공배수를 구하여 반환합니다.
+    public static int lcm(int a, int b) {
+        int gcd = 1;
+        for(int i = 1; i <= Math.min(a, b); i++) {
+            if(a % i == 0 && b % i == 0)
+                gcd = i;
+        }
+    
+        return a * b / gcd;
+    }
+    
+    // index번째 까지 인덱스의 숫자 중에 가장 큰 값을 반환합니다.
+    public static int getLCMAll(int index) {
+        // 남은 원소가 1개라면 그 자신이 답이 됩니다.
+        if(index == 1)
+            return arr[1];
+    
+        // 1번째 ~ index - 1번째 원소의 최소공배수를 구한 결과와
+        // 현재 index 원소의 최소공배수를 구하여 반환합니다.
+        return lcm(getLCMAll(index - 1), arr[index]);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++)
+        // 변수 선언 및 입력:
+        n = sc.nextInt();
+        for(int i = 1; i <= n; i++)
             arr[i] = sc.nextInt();
-        // Please write your code here.
-        System.out.println(LCM(n-1,arr));
-    }
 
-    public static int LCM(int i,int[] arr){
-        int curVal = arr[i];
-        if(i==0) return curVal;
-        
-        //현재까지의 최소공배수와 나누어 떨어지면 더 큰값
-        if(canDivide(curVal,LCM(i-1,arr))) return Math.max(curVal,LCM(i-1,arr));
-        //나누어 떨어지지 않으면 두개를 곱한 값을 최대공약수로 나눔
-        else return curVal*LCM(i-1,arr)/GCD(curVal,LCM(i-1,arr));
-    }
-
-    public static int GCD(int i,int j){
-        if(i%j==0) return j;
-        return GCD(j,i%j);
-    }
-    public static boolean canDivide(int i,int j){
-        return i%j==0 || j%i==0;
+        // 모든 수의 최소공배수를 구합니다.
+        System.out.print(getLCMAll(n));
     }
 }
