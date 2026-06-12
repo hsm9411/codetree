b80900f9142225b0dfdde66859e81c566e9abae8
hsm9411 <210804140+hsm9411@users.noreply.github.com>
2026-06-12 17:04:45 +0900

    [Passed] 좌표평면 위의 특정 구역 2 (162ms, 10MB)

--- trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
+++ trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.java
@@ -8,12 +8,13 @@ public class Main {
             arr[i][0]=sc.nextInt();
             arr[i][1]=sc.nextInt();
         }
-        int minx = 1;
-        int maxx = 40000;
-        int miny = 1;
-        int maxy = 40000;
+
         int min = Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
+            int minx = 1;
+            int maxx = 40000;
+            int miny = 1;
+            int maxy = 40000;
             for(int j=0;j<n;j++){
                 if(i==j) continue;
                 minx= Math.max(arr[j][0], minx);
@@ -22,10 +23,6 @@ public class Main {
                 maxy= Math.min(arr[j][1], maxy);
             }
             min = Math.min((maxx-minx)*(maxy-miny),min);
-            minx = 1;
-            maxx = 40000;
-            miny = 1;
-            maxy = 40000;
         }
         System.out.println(min);
     }
