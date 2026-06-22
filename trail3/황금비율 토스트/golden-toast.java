1a5484f60133e472e29d8f0d97067aad678ca458
hsm9411 <210804140+hsm9411@users.noreply.github.com>
2026-06-22 11:11:29 +0900

    [Passed] 황금비율 토스트 (337ms, 19MB)

--- trail3/황금비율 토스트/golden-toast.java
+++ trail3/황금비율 토스트/golden-toast.java
@@ -1,68 +1,52 @@
 import java.util.Scanner;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
+    public static int n, m;
+    public static String s;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        // 1. 식빵의 개수 n과 명령어의 개수 m 입력
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        
-        // 2. 초기 식빵 문자열 입력
-        String str = sc.next();
-        
-        // 커서를 기준으로 왼쪽과 오른쪽을 나눌 스택
-        Stack<Character> leftStack = new Stack<>();
-        Stack<Character> rightStack = new Stack<>();
-        
-        // 초기에는 가리키는 위치(커서)가 맨 뒤에 있으므로 모든 빵을 왼쪽 스택에 넣음
-        for (int i = 0; i < n; i++) {
-            leftStack.push(str.charAt(i));
-        }
-        
-        // 3. m개의 명령어 처리
-        for (int i = 0; i < m; i++) {
-            String command = sc.next(); // 명령어(L, R, D, P) 읽기
-            
-            if (command.equals("L")) {
-                // 커서를 왼쪽으로 이동
-                if (!leftStack.isEmpty()) {
-                    rightStack.push(leftStack.pop());
-                }
-            } else if (command.equals("R")) {
-                // 커서를 오른쪽으로 이동
-                if (!rightStack.isEmpty()) {
-                    leftStack.push(rightStack.pop());
-                }
-            } else if (command.equals("D")) {
-                // 커서 바로 뒤(오른쪽)에 있는 빵 제거
-                if (!rightStack.isEmpty()) {
-                    rightStack.pop();
-                }
-            } else if (command.equals("P")) {
-                // 커서 위치에 새로운 빵(문자) 추가
-                // P 명령어 뒤에 띄어쓰기 되어있는 문자 읽기 (예: "P s"에서 's'를 읽음)
-                String pStr = sc.next(); 
-                leftStack.push(pStr.charAt(0));
+
+        // 입력:
+        n = sc.nextInt();
+        m = sc.nextInt();
+        s = sc.next();
+
+        // 연결리스트 정의
+        LinkedList<Character> l = new LinkedList<>();
+        for(int i = 0; i < s.length(); i++)
+            l.add(s.charAt(i));
+        // Iterator 정의 (맨 끝 위치에서 시작하기 위해서는 listIterator안에 l.size() 필요)
+        ListIterator<Character> it = l.listIterator(l.size());
+
+        while(m-- > 0) {
+            char command = sc.next().charAt(0);
+
+            if(command == 'L') {
+                if(it.hasPrevious()) // 빵들의 맨 앞이 아니라면
+                    it.previous();   // 앞으로 이동합니다.
+            } 
+            else if(command == 'R') {
+                if(it.hasNext())     // 빵들의 맨 뒤가 아니라면
+                    it.next();       // 뒤로 이동합니다.
             }
+            else if(command == 'D') {
+                if(it.hasNext()) {   // 빵들의 맨 뒤가 아니라면
+                    it.next();
+                    it.remove();     // 바로 뒤에 있는 빵을 제거합니다.
+                }
+            }
+            else if(command == 'P') {
+                char c = sc.next().charAt(0);
+                it.add(c);           // 가리키는 위치에 문자 c를 추가합니다.
+            }    
         }
-        
-        // 4. 출력 준비
-        // 정방향으로 출력하기 위해 왼쪽 스택에 남은 문자들을 전부 오른쪽 스택으로 넘김
-        while (!leftStack.isEmpty()) {
-            rightStack.push(leftStack.pop());
-        }
-        
-        // 오른쪽 스택에서 하나씩 꺼내어 결과 문자열 만들기
-        StringBuilder sb = new StringBuilder();
-        while (!rightStack.isEmpty()) {
-            sb.append(rightStack.pop());
-        }
-        
-        // 결과 출력
-        System.out.println(sb.toString());
-        
-        sc.close();
+
+        // 출력:
+        it = l.listIterator();
+        while(it.hasNext())
+            System.out.print(it.next());
     }
 }
